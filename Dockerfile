6355708756d6e2aef3963084e772233e49d4b1cf
Anders Gjermshus <anders.gjermshus@hotmail.com>
2017-05-07 03:23:13 +0200

    Bugfix in environemnt variables for docker

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,20 @@
 FROM mhart/alpine-node:latest
 
-COPY package.json /vstsbot/package.json
-RUN cd /vstsbot; npm install --production
+RUN npm install -g gulp-cli
 
-COPY output /vstsbot
+COPY . /vstsbotsrc
+RUN cd /vstsbotsrc; npm install --production
+
+RUN npm install gulp
+WORKDIR /vstsbotsrc
+
+RUN gulp
+RUN cp -r /vstsbotsrc/output /vstsbot
+RUN rm /vstsbot/config.json
+RUN cp -r /vstsbotsrc/node_modules /vstsbot
+
+WORKDIR /vstsbot
+
+RUN rm -rf /vstsbotsrc
 
 CMD ["node", "/vstsbot/app.js"]
--- src/app.ts
+++ src/app.ts
@@ -12,12 +12,14 @@ if (existsSync(join(__dirname,"config.json"))) {
     config = JSON.parse(fileContent);
 }
 
-config.BotName = process.env.BotName || config.BotName;
-config.SlackToken = process.env.SlackToken || config.SlackToken;
-config.VSTSTeamProject = process.env.VSTSTeamProject || config.VSTSTeamProject;
-config.VSTSUsername = process.env.VSTSUsername || config.VSTSUsername;
-config.VSTSPassword = process.env.VSTSPassword || config.VSTSPassword;
-config.VSTSDomain = process.env.VSTSDomain || config.VSTSDomain;
+let runConfig: IVSTSBotConfig = {
+    BotName: process.env.BotName || config.BotName,
+    SlackToken: process.env.SlackToken || config.SlackToken,
+    VSTSTeamProject: process.env.VSTSTeamProject || config.VSTSTeamProject,
+    VSTSUsername: process.env.VSTSUsername || config.VSTSUsername,
+    VSTSPassword: process.env.VSTSPassword || config.VSTSPassword,
+    VSTSDomain: process.env.VSTSDomain || config.VSTSDomain
+};
 
-var bot: VSTSBot = new VSTSBot(config);
+var bot: VSTSBot = new VSTSBot(runConfig);
 bot.Run();
