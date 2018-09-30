4cb10d3755fc9436d34879c68adc2a374584c449
gjermshus <gjermshus@gmail.com>
2018-09-30 22:13:06 +0200

    Upgrade to gulp 4.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
-var gulp = require("gulp"),
-    tsc = require("gulp-typescript"),
+var gulp = require('gulp'),
+    tsc = require('gulp-typescript'),
     sourcemaps = require('gulp-sourcemaps'),
     clean = require('gulp-clean'),
     util = require('gulp-util');
@@ -7,21 +7,26 @@ var gulp = require("gulp"),
 var isProductionBuild = !!util.env.production;
 console.log('Is Production Build? ', isProductionBuild);
 
-gulp.task("ts", () => {
-    var tsProject = tsc.createProject("./tsconfig.json");
-    return gulp.src("./src/**/*.ts")
+gulp.task('ts', () => {
+    var tsProject = tsc.createProject('./tsconfig.json');
+    return gulp
+        .src('./src/**/*.ts')
         .pipe(!isProductionBuild ? sourcemaps.init() : util.noop())
         .pipe(tsProject(tsc.reporter.defaultReporter()))
-        .pipe(!isProductionBuild ? sourcemaps.write("./maps") : util.noop())
-        .pipe(gulp.dest("./output/"));
+        .pipe(!isProductionBuild ? sourcemaps.write('./maps') : util.noop())
+        .pipe(gulp.dest('./output/'));
 });
 
-gulp.task("copyConfig", () => {
-    return gulp.src("./src/config.json").pipe(!isProductionBuild ? gulp.dest("./output/") : util.noop());
+gulp.task('copyConfig', () => {
+    return gulp
+        .src('./src/config.test.json', { allowEmpty: true })
+        .pipe(
+            !isProductionBuild ? gulp.dest('./output/config.json') : util.noop()
+        );
 });
 
 gulp.task('cleanOutput', () => {
-    return gulp.src('./output').pipe(clean());
-})
+    return gulp.src('./output', { allowEmpty: true }).pipe(clean());
+});
 
-gulp.task("default", ['cleanOutput', "ts", "copyConfig"]);
+gulp.task('default', gulp.series('cleanOutput', 'ts', 'copyConfig'));
